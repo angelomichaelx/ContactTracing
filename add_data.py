6666d9a8c3c780c90e332b89354111b73e23f416
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-28 06:05:49 +0800

    title

--- add_data.py
+++ add_data.py
@@ -11,3 +11,7 @@ class add_data(tk.Frame):
     def __init__(self, master = None):
         super().__init__(master)
         self.configure(bg = "light blue")
+
+# title
+        self.personal = tk.Label(self, text = "CONTACT TRACING FORM", font=("Times", 20), bg = "light blue")
+        self.personal.place(x = 180, y = 5)
