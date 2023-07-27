1bd235a58e61a8345427e43aaea511a43d47deae
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-28 05:58:47 +0800

    creating class

--- add_data.py
+++ add_data.py
@@ -5,3 +5,9 @@
 import tkinter as tk
 import csv
 from tkinter import messagebox
+
+#creating class
+class add_data(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.configure(bg = "light blue")
