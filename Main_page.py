60dd4a343492295c6599f975f6933045c7668052
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-28 14:06:32 +0800

    creating class and title and assigning size

--- Main_page.py
+++ Main_page.py
@@ -5,3 +5,12 @@
 import tkinter as tk
 from add_data import add_data
 from search_area import search_area
+#creating class
+class main_page(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #title
+        self.title("COVID-14 Contact Tracing APP")
+        #size
+        self.geometry("780x600")
+        self.configure(bg = "light blue")
