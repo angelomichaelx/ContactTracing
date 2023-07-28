0406362aecb2ac2b2e3378cb1ca091ce73f3dfad
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-07-28 14:38:04 +0800

    data information files and printinmg entries

--- search_area.py
+++ search_area.py
@@ -23,3 +23,31 @@ class search_area(tk.Frame):
 
         self.search_button = tk.Button(self, text= "  Search  ", command = self.search_name, bg = "white")
         self.search_button.place(x = 385, y = 100)
+
+    def search_name(self):
+        name_entry = self.search_entry.get()
+
+        #data informationg files
+        with open("information.csv", "r", newline = "") as file:
+            reader = csv.reader(file)
+            found_entries = []
+            for row in reader:
+                if name_entry in row[0]:
+                    found_entries.append(row)
+
+        #print
+        if found_entries:
+            for i, entry in enumerate(found_entries):
+                result = f"Name: {entry[0]}\n"
+                result += f"Gender: {entry[1]}\n"
+                result += f"Date of Birth: {entry[2]}\n"
+                result += f"Age: {entry[3]}\n"
+                result += f"Email Address: {entry[4]}\n"
+                result += f"Name of City: {entry[5]}\n"
+                result += f"Vaccination Status: {entry[6]}\n"
+                result += f"Symptoms of COVID-19: {entry[7]}\n"
+                result += f"Exposure to COVID-19: {entry[8]}\n"
+                result += f"COVID-19 Test Status: {entry[9]}"
+
+                result = tk.Label(text = result, font = ("Arial", 20) , justify=("left"), bg = "light blue")
+                result.place(x = 180, y = 200)
